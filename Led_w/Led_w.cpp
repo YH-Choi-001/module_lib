c7e191436d075155ec8a60c799d0d249de94668b
YH-Choi-001 <yhei.choi@gmail.com>
2022-01-24 23:13:42 +0800

    replaces Led_w to a more work saving logic

--- Led_w/Led_w.cpp
+++ Led_w/Led_w.cpp
@@ -34,23 +34,29 @@ Led_w_fast(init_led_w_pin)
 }
 
 void yh::rec::Led_w::led_on () {
-    (*led_w_pin_output_port) |= led_w_pin_mask; // this line replaces digitalWrite(led_w_pin, HIGH);
-    led_state = true;
+    if (!((*led_w_pin_output_port) & led_w_pin_mask))
+        (*led_w_pin_output_port) |= led_w_pin_mask; // this line replaces digitalWrite(led_w_pin, HIGH);
 }
 
 void yh::rec::Led_w::led_off () {
+    if ((*led_w_pin_output_port) & led_w_pin_mask)
     (*led_w_pin_output_port) &= ~led_w_pin_mask; // this line replaces digitalWrite(led_w_pin, LOW);
-    led_state = false;
 }
 
 void yh::rec::Led_w::set_led (const bool assign_led_state) {
     // this line below replaces digitalWrite(led_w_pin, assign_led_state);
-    (led_state = assign_led_state) ? ((*led_w_pin_output_port) |= led_w_pin_mask) : ((*led_w_pin_output_port) &= ~led_w_pin_mask);
+    // orig, new
+    //    0    0 => nothing
+    //    0    1 => turn_on
+    //    1    0 => turn_off
+    //    1    1 => nothing
+    const bool curr_state = ((*led_w_pin_output_port) & led_w_pin_mask)?1:0;
+    if ((assign_led_state?1:0) ^ curr_state)
+        curr_state ? ((*led_w_pin_output_port) &= ~led_w_pin_mask) : ((*led_w_pin_output_port) |= led_w_pin_mask);
 }
 
 void yh::rec::Led_w::toggle_led () {
-    // this line below replaces digitalWrite(led_w_pin, led_state = !led_state);
-    (led_state = !led_state) ? ((*led_w_pin_output_port) |= led_w_pin_mask) : ((*led_w_pin_output_port) &= ~led_w_pin_mask);
+    ((*led_w_pin_output_port) & led_w_pin_mask) ? ((*led_w_pin_output_port) &= ~led_w_pin_mask) : ((*led_w_pin_output_port) |= led_w_pin_mask);
 }
 
 #endif // #ifndef LED_W_CPP
