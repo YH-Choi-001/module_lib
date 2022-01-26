84367e40dc55b331445ab9f0aa1f90f17cb07cd4
YH-Choi-001 <yhei.choi@gmail.com>
2022-01-27 03:10:35 +0800

    Serial com (main version)

---
+++ UART_com/UART_com.cpp
@@ -0,0 +1,214 @@
+#ifndef UART_COM_CPP
+#define UART_COM_CPP __DATE__ ", " __TIME__
+
+#include "UART_com.h"
+
+yh::rec::UART_com::UART_com (Serial_ &init_serial_obj) :
+    uart_serial(init_serial_obj), request_timeout(0)
+{
+    //
+}
+
+inline void yh::rec::UART_com::begin (const uint32_t baud) {
+    uart_serial.begin(baud);
+}
+
+// ===================================================================================
+// ========================= protected: direct communication =========================
+// ===================================================================================
+
+// write a key to replying arduino
+void yh::rec::UART_com::write_key (const uint8_t key) {
+    uart_serial.write(key);
+}
+
+// write a byte of data to replying arduino
+void yh::rec::UART_com::write_data (const uint8_t key, const uint8_t data) {
+    uart_serial.write(key);
+    uart_serial.write(data);
+}
+
+// write 2 bytes of data to replying arduino
+void yh::rec::UART_com::write_data (const uint8_t key, const uint8_t data0, const uint8_t data1) {
+    uart_serial.write(key);
+    uart_serial.write(data0);
+    uart_serial.write(data1);
+}
+
+// request a byte from writing the key to replying arduino
+uint8_t yh::rec::UART_com::request_data (const uint8_t key) {
+    uart_serial.write(key);
+    while (uart_serial.available() < 1) {}
+    return uart_serial.read();
+}
+
+// request a byte from writing the key to replying arduino, with a timeout
+uint8_t yh::rec::UART_com::request_data (const uint8_t key, const unsigned long timeout) {
+    uart_serial.write(key);
+    const unsigned long init_micros = micros();
+    while (uart_serial.available() < 1) {
+        if ((micros() - init_micros) > timeout) {
+            request_timeout = 1;
+            return 0;
+        }
+    }
+    return uart_serial.read();
+}
+
+// request a byte from writing the key to replying arduino
+uint16_t yh::rec::UART_com::request_2_data (const uint8_t key) {
+    uart_serial.write(key);
+    while (uart_serial.available() < 2) {}
+    return (uart_serial.read() << 8) | uart_serial.read();
+}
+
+// request a byte from writing the key to replying arduino, with a timeout
+uint16_t yh::rec::UART_com::request_2_data (const uint8_t key, const unsigned long timeout) {
+    uart_serial.write(key);
+    const unsigned long init_micros = micros();
+    while (uart_serial.available() < 2) {
+        if ((micros() - init_micros) > timeout) {
+            request_timeout = 1;
+            return 0;
+        }
+    }
+    return (uart_serial.read() << 8) | uart_serial.read();
+}
+
+// ===================================================================================
+// =================================== grayscales ====================================
+// ===================================================================================
+
+uint8_t yh::rec::UART_com::grayscales_8_sides_touch_white () {
+    return request_data(GRYSCL_8_DIR);
+}
+
+uint8_t yh::rec::UART_com::hori_grayscales_touch_white () {
+    return request_data(HORI_GRYSCL);
+}
+
+uint8_t yh::rec::UART_com::vert_grayscales_touch_white () {
+    return request_data(VERT_GRYSCL);
+}
+
+void yh::rec::UART_com::request_calibrate_grayscales () {
+    write_key(CAL_GRYSCL);
+}
+
+// ===================================================================================
+// ================================== compoundeyes ===================================
+// ===================================================================================
+
+uint16_t yh::rec::UART_com::sun_direction_HIGH () {
+    return request_data(SUN_DIR_H) << 2;
+}
+
+uint16_t yh::rec::UART_com::sun_intensity_HIGH () {
+    return request_data(SUN_VAL_H) << 2;
+}
+
+uint8_t yh::rec::UART_com::sun_direction_LOW () {
+    return request_data(SUN_DIR_L);
+}
+
+uint8_t yh::rec::UART_com::sun_intensity_LOW () {
+    return request_data(SUN_VAL_L);
+}
+
+// ===================================================================================
+// =================================== ultrasounds ===================================
+// ===================================================================================
+
+uint16_t yh::rec::UART_com::find_ball_ultrasound_HIGH () {
+    return request_data(FB_UTS_H, 688) << 2;
+}
+
+uint8_t yh::rec::UART_com::find_ball_ultrasound_LOW () {
+    return request_data(FB_UTS_L, 688);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_0_HIGH () {
+    return request_data(UTS0_H) << 2;
+}
+
+uint8_t yh::rec::UART_com::ultrasound_0_LOW () {
+    return request_data(UTS0_L);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_1_HIGH () {
+    return request_data(UTS1_H) << 2;
+}
+
+uint8_t yh::rec::UART_com::ultrasound_1_LOW () {
+    return request_data(UTS1_L);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_2_HIGH () {
+    return request_data(UTS2_H) << 2;
+}
+
+uint8_t yh::rec::UART_com::ultrasound_2_LOW () {
+    return request_data(UTS2_L);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_3_HIGH () {
+    return request_data(UTS3_H) << 2;
+}
+
+uint8_t yh::rec::UART_com::ultrasound_3_LOW () {
+    return request_data(UTS3_L);
+}
+
+uint16_t yh::rec::UART_com::find_ball_ultrasound_9bit () {
+    return request_2_data(FB_UTS_9, 688);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_0_9bit () {
+    return request_2_data(UTS0_9);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_1_9bit () {
+    return request_2_data(UTS1_9);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_2_9bit () {
+    return request_2_data(UTS2_9);
+}
+
+uint16_t yh::rec::UART_com::ultrasound_3_9bit () {
+    return request_2_data(UTS3_9);
+}
+
+// ===================================================================================
+// ===================================== compass =====================================
+// ===================================================================================
+
+uint16_t yh::rec::UART_com::compass_HIGH () {
+    return request_data(CMPAS_H) << 2;
+}
+
+uint8_t yh::rec::UART_com::compass_LOW () {
+    return request_data(CMPAS_L);
+}
+
+uint16_t yh::rec::UART_com::compass_9bit () {
+    return request_2_data(CMPAS_9);
+}
+
+void yh::rec::UART_com::reset_compass () {
+    write_data(RST_CMPAS_0);
+}
+
+void yh::rec::UART_com::set_compass_direction (const uint16_t direction) {
+    write_data(SET_CMPAS, (direction >> 2) & 0xff, (direction & 0xff));
+}
+
+// ===================================================================================
+// ==================================== dribbler =====================================
+// ===================================================================================
+
+void yh::rec::UART_com::set_dribbler_spd (const int8_t spd) {
+    write_data(DRBLR_SPD, spd);
+}
+
+#endif // #ifndef UART_COM_CPP
---
+++ UART_com/UART_com.h
@@ -0,0 +1,169 @@
+#ifndef UART_COM_H
+#define UART_COM_H __DATE__ ", " _TIME__
+
+// declares the class Serial_ to prevent VS_code from reporting errors of undeclared types
+class Serial_;
+
+#if defined(ARDUINO) && !defined(Arduino_h)
+#include <Arduino.h>
+#endif // #if defined(ARDUINO) && !defined(Arduino_h)
+
+namespace yh {
+    namespace rec {
+        // note that if values not on this table are transmitted,
+        // the replying arduino may have undefined behaviours
+        // note that items that have the name \"9-bit\" actually
+        // transmit 10 bits, which the MSB is reserved for data error indication
+        enum UART_com_keys_t {
+            // capital letters first
+            FB_UTS_H       = 'A', // find-ball ultrasound HIGH byte
+            // SET_BAUD       = 'B', // set baud rate
+            CMPAS_H        = 'C', // compass HIGH byte
+            DRBLR_SPD      = 'D', // set dribbler speed (-1, 0, 1)
+            SUN_DIR_H      = 'E', // the direction of the strongest ball (Sun) HIGH byte
+            SUN_VAL_H      = 'F', // the intensity of the strongest ball (Sun) HIGH byte
+            GRYSCL_8_DIR   = 'G', // 8 simple directions of grayscale (F, LF, L, LB, RF, R, RB, B)
+            HORI_GRYSCL    = 'H', // horizontal grayscales (backup)
+            CMPAS_9        = 'K', // 9-bit compass
+            FB_UTS_9       = 'L', // 9-bit find-ball ultrasound
+            UTS0_9         = 'M', // 9-bit ultrasound 0
+            UTS1_9         = 'N', // 9-bit ultrasound 1
+            UTS2_9         = 'O', // 9-bit ultrasound 2
+            UTS3_9         = 'P', // 9-bit ultrasound 3
+            RST_CMPAS_0    = 'R', // reset compass to 0
+            SET_CMPAS      = 'S', // set compass to an inputed direction
+            VERT_GRYSCL    = 'V', // vertical grayscales (backup)
+            UTS0_H         = 'W', // ultrasound 0 HIGH byte
+            UTS1_H         = 'X', // ultrasound 1 HIGH byte
+            UTS2_H         = 'Y', // ultrasound 2 HIGH byte
+            UTS3_H         = 'Z', // ultrasound 3 HIGH byte
+            // small letters next
+            FB_UTS_L       = 'a', // find-ball ultrasound LOW byte
+            CMPAS_L        = 'c', // compass LOW byte
+            SUN_DIR_L      = 'e', // the direction of the strongest ball (Sun) LOW byte
+            SUN_VAL_L      = 'f', // the intensity of the strongest ball (Sun) LOW byte
+            CAL_GRYSCL     = 'g', // request calibrate grayscales
+            UTS0_L         = 'w', // ultrasound 0 LOW byte
+            UTS1_L         = 'x', // ultrasound 1 LOW byte
+            UTS2_L         = 'y', // ultrasound 2 LOW byte
+            UTS3_L         = 'z'  // ultrasound 3 LOW byte
+            // the priority of returning data from replying arduino:
+                // 0. grayscale // should not access grayscale through UART, but digital pins
+                // 1. compass     (HIGH > 9-bit > LOW)
+                // 2. compoundeye (HIGH > LOW)
+                // 3. ultrasound  (HIGH > 9-bit > LOW)
+            // then the priority of configuring data in replying aruino:
+                // 4. set motor speed
+                // 5. set dribbler speed
+                // 6. calibrate grayscale
+                // 7. software reset compass
+                // 8. software set compass direction
+        };
+        class UART_com {
+            private:
+                //
+            protected:
+                // the selected UART serial object
+                Serial_ &uart_serial;
+                // write a key to replying arduino
+                void write_key (const uint8_t key);
+                // write a byte of data to replying arduino
+                void write_data (const uint8_t key, const uint8_t data);
+                // write 2 bytes of data to replying arduino
+                void write_data (const uint8_t key, const uint8_t data0, const uint8_t data1);
+                // request a byte from writing the key to replying arduino
+                uint8_t request_data (const uint8_t key);
+                // request a byte from writing the key to replying arduino, with a timeout
+                uint8_t request_data (const uint8_t key, const unsigned long timeout);
+                // request 2 bytes from writing the key to replying arduino
+                uint16_t request_2_data (const uint8_t key);
+                // request a byte from writing the key to replying arduino, with a timeout
+                uint16_t request_2_data (const uint8_t key, const unsigned long timeout);
+                // flag to indicate if request_data(..., timeout) or request_2_data(..., timeout) has occured a time-out
+                unsigned int request_timeout : 1;
+            public:
+                // inits the object of UART serial communication to this object
+                // can be Serial, Serial1, Serial2, Serial3 for arduino mega boards
+                // depends on which UART you have connected to
+                UART_com (Serial_ &init_serial_obj);
+                // calls uart_serial.begin(baud) and sets the baud rate
+                inline void begin (const uint32_t baud);
+
+                // grayscales:
+
+                // returns if grayscales of (F, LF, L, LB, RF, R, RB, B) direction are touching the white line
+                uint8_t grayscales_8_sides_touch_white ();
+                // returns if grayscales of L0, L1, L2, L3, R0, R1, R2, R3 are touching the white line
+                uint8_t hori_grayscales_touch_white ();
+                // returns if grayscales of F0, F1, F2, F3, B0, B1, B2, B3 are touching the white line
+                uint8_t vert_grayscales_touch_white ();
+                // holds the thread and waits for the calibration of grayscales to be completed
+                void request_calibrate_grayscales ();
+
+                // compoundeyes:
+
+                // returns the direction of the Sun with a resolution of 4degrees ( << 2 )
+                uint16_t sun_direction_HIGH ();
+                // returns the intensity of the Sun with a resolution of 4units_of_brightness ( << 2 )
+                uint16_t sun_intensity_HIGH ();
+                // returns the direction of the Sun % 256
+                uint8_t sun_direction_LOW ();
+                // returns the intensity of the Sun % 256
+                uint8_t sun_intensity_LOW ();
+
+                // ultrasounds:
+
+                // returns the distance in cm with a resolution of 4cm ( << 2 )
+                uint16_t find_ball_ultrasound_HIGH ();
+                // returns the distance in cm % 256
+                uint8_t find_ball_ultrasound_LOW ();
+                // returns the distance in cm with a resolution of 4cm ( << 2 )
+                uint16_t ultrasound_0_HIGH ();
+                // returns the distance in cm % 256
+                uint8_t ultrasound_0_LOW ();
+                // returns the distance in cm with a resolution of 4cm ( << 2 )
+                uint16_t ultrasound_1_HIGH ();
+                // returns the distance in cm % 256
+                uint8_t ultrasound_1_LOW ();
+                // returns the distance in cm with a resolution of 4cm ( << 2 )
+                uint16_t ultrasound_2_HIGH ();
+                // returns the distance in cm % 256
+                uint8_t ultrasound_2_LOW ();
+                // returns the distance in cm with a resolution of 4cm ( << 2 )
+                uint16_t ultrasound_3_HIGH ();
+                // returns the distance in cm % 256
+                uint8_t ultrasound_3_LOW ();
+
+                // returns the distance in cm with a resolution of 1cm
+                uint16_t find_ball_ultrasound_9bit ();
+                // returns the distance in cm with a resolution of 1cm
+                uint16_t ultrasound_0_9bit ();
+                // returns the distance in cm with a resolution of 1cm
+                uint16_t ultrasound_1_9bit ();
+                // returns the distance in cm with a resolution of 1cm
+                uint16_t ultrasound_2_9bit ();
+                // returns the distance in cm with a resolution of 1cm
+                uint16_t ultrasound_3_9bit ();
+
+                // compass:
+
+                // returns the direction of field_north with a resolution of 4degrees ( << 2 )
+                uint16_t compass_HIGH ();
+                // returns the direction of field_north % 256
+                uint8_t compass_LOW ();
+                // returns the direction of field_north with a resolution of 1degree
+                uint16_t compass_9bit ();
+                // resets the field_north of the compass to current facing direction
+                void reset_compass ();
+                // sets the current facing direction to an inputed direction
+                void set_compass_direction (const uint16_t direction);
+
+                // dribbler:
+
+                // sets the speed of the dribbler to (clockwise or anti-clockwise with full-power) or (0)
+                void set_dribbler_spd (const int8_t spd);
+        };
+    }
+}
+
+#endif // #ifndef UART_COM_H
