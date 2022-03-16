36684f42c354b54733f6e2fde250dbc5bf35e422
YH-Choi-001 <yhei.choi@gmail.com>
2022-03-16 22:19:18 +0800

    updated comments

--- Pcint/Pcint.h
+++ Pcint/Pcint.h
@@ -6,9 +6,12 @@
 #endif // #if defined(ARDUINO) && !defined(Arduino_h)
 
 void setup_pcint (const uint8_t pin) {
-    *digitalPinToPCMSK(pin) |= (1 << digitalPinToPCMSKbit(pin));  // enable pin
+    // Pin Change Interrupt - single pin Mask
+    *digitalPinToPCMSK(pin) |= (1 << digitalPinToPCMSKbit(pin));  // enable specific pin in the PCINT group to trigger the interrupt
+    // Pin Change Interrupt Flag Register
     // the respective bit of PCIFR register is set to 1 when a pin in the port changes
     PCIFR |= (1 << digitalPinToPCICRbit(pin)); // clear previous interrupts
+    // Pin Change Interrupt Control Register
     PCICR |= (1 << digitalPinToPCICRbit(pin)); // enable interrupt for the group
 }
 
