b7109d31feaf6ace7313e8d7eaeced0d18c72b7a
YH-Choi-001 <yhei.choi@gmail.com>
2022-04-18 21:01:24 +0100

    improved example

--- Hc_sr04/examples/test_pcint/test_pcint.ino
+++ Hc_sr04/examples/test_pcint/test_pcint.ino
@@ -15,7 +15,6 @@ ISR(PCINT2_vect) {
 void setup_pcint (const uint8_t pin) {
     *digitalPinToPCMSK(pin) |= (1 << digitalPinToPCMSKbit(pin));  // enable pin
     // the respective bit of PCIFR register is set to 1 when a pin in the port changes
-    PCIFR |= (1 << digitalPinToPCICRbit(pin)); // clear previous interrupts
     PCICR |= (1 << digitalPinToPCICRbit(pin)); // enable interrupt for the group
 }
 
