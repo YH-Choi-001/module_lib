b339f5825d7e710a2603b9d46544bb7504ce65f6
YH-Choi-001 <yhei.choi@gmail.com>
2022-07-24 12:03:11 +0800

    pid self-timing update_corr() method added

--- pid/pid.h
+++ pid/pid.h
@@ -17,24 +17,18 @@ namespace yh {
                 double p, i, d;
                 // weighting of p, i, d correction
                 double p_weighting, i_weighting, d_weighting;
+                //
+                unsigned long prev_time;
             public:
                 // default constructor to init 0 for basic values, and 1 for default weighting values
-                Pid () : prev_error(0), p(0), i(0), d(0), p_weighting(1), i_weighting(1), d_weighting(1) {}
+                Pid () : prev_error(0), p(0), i(0), d(0), p_weighting(1), i_weighting(1), d_weighting(1), prev_time(0) {}
                 // inits the 3 arguments to the respective weighting values
                 Pid (const double init_p_weight, const double init_i_weight, const double init_d_weight) :
-                prev_error(0), p(0), i(0), d(0), p_weighting(init_p_weight), i_weighting(init_i_weight), d_weighting(init_d_weight) {}
-                // void update_pid_val (const double input_target_val, const double input_reality_val) {
-                //     target_val = input_target_val;
-                //     reality_val = input_reality_val;
-                //     error = reality_val - target_val; // error is +ive when reality_val > target_val, it is -ive when reality < target_val
-                //     p = -error; // definition of p: p == -error
-                //     const double duration = micros() - previous_micros;
-                //     i += (-error) * duration; // i == the area under the curve of the error graph 積分
-                //     d = -(error - previous_error) / duration; // d == rate of change of error in the error graph 微分
-                //     previous_micros += duration;
-                //     previous_error = error;
-                // }
-
+                prev_error(0), p(0), i(0), d(0), p_weighting(init_p_weight), i_weighting(init_i_weight), d_weighting(init_d_weight), prev_time(0) {}
+                // kicks off the prev_time
+                void begin () {
+                    prev_time = micros();
+                }
                 // sets the p_weighting value
                 inline void set_p_weighting (const double request_p_weighting) __attribute__((__always_inline__)) {
                     p_weighting = (request_p_weighting < 0) ? 0 : request_p_weighting;
@@ -62,6 +56,17 @@ namespace yh {
                     d = (p - prev_error) / time_diff * d_weighting;
                     prev_error = p;
                     p *= p_weighting;
+                    return p + i + d;
+                }
+                // updates the correction value with built-in time tracker
+                double update_corr (const double target_val, const double actual_val) {
+                    const unsigned long time_diff = micros() - prev_time;
+                    p = target_val - actual_val;
+                    i += p * time_diff * i_weighting;
+                    d = (p - prev_error) / time_diff * d_weighting;
+                    prev_error = p;
+                    p *= p_weighting;
+                    prev_time += time_diff;
                     return p + i + d;
                 }
                 // resets the i correction value to 0
